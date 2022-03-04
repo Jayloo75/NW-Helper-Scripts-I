3fdff8e12fbf356a266d41a1053b96c0dbf95bdf
jason@jayloo.com <T@ngier69>
2022-03-04 09:28:57 -0600

    Moving search files to own directory

--- Auctioneer.py
+++ Auctioneer.py
@@ -8,7 +8,9 @@ from tkinter import *
 # from tkinter import scrolledtext
 from functools import partial
 
-tp_items = [[1, "Oil", "Dark gold"], [2, "IronOre", "Lowest level ore"], [2, "Greenwood", "Easy Wood"]]
+tp_items = [[1, "Oil", "Dark gold", "auctioneer-search-dropdown-oil.png"],
+            [2, "Iron Ore", "Lowest level ore", "auctioneer-search-dropdown-oil.png"],
+            [3, "Green Wood", "Easy Wood", "auctioneer-search-dropdown-oil.png"]]
 
 
 def clicked(windows_obj, search_term_id):
@@ -20,40 +22,9 @@ def clicked(windows_obj, search_term_id):
     pyautogui.write("testing 1=" + str(tp_items[search_term_id][1]))
 
 
-def main():
-    """
-    Main function for the program
-    """
-    print("main function Initiated")
-
-
-    # Initiate windows class
-    windows_obj = Windows()
-
-    window = Tk()
-    window.title("New World Messages App")
-    window.geometry('250x500')
-
-    row_counter = 0
-    for item in tp_items:
-        print(item)
-        the_text = item[1], "#", row_counter
-        # btn = tk.Button(my_w, text=language, command=lambda lan=language:show_lan(lan))
-        # Button(window, text=the_text, command=lambda: clicked(windows_obj, row_counter)).grid(column=0, row=row_counter)
-        # Button(window, text=the_text, command=partial(clicked, windows_obj, row_counter)).grid(column=0, row=row_counter)
-        Button(window, text=the_text, command=lambda win=windows_obj, rc=row_counter: clicked(win, rc)).grid(column=0, row=row_counter)
-
-        # tk.Button(self.board, command=lambda i=i, j=j: self.on_click(i, j))
-
-        row_counter = row_counter + 1
-
-    # btn1 = Button(window, text="Oil", command=lambda: clicked(windows_obj, "Oil"))
-    # btn1.grid(column=0, row=0)
-    #
-    # btn2 = Button(window, text="Iron Ore", command=lambda: clicked(windows_obj, "Iron Ore"))
-    # btn2.grid(column=0, row=1)
-
-    window.mainloop()
+def search_items(windows_obj, search_term_id):
+    print("debug---", search_term_id)
+    print("debug---", tp_items[search_term_id][1])
 
     newWorldWindow = windows_obj.newWorldWindow
     RegionX = round(newWorldWindow.left + 627)
@@ -70,9 +41,7 @@ def main():
     RegionWidth = round(RegionX + 467)
     RegionHeight = round(RegionY + 495)
     region_auctioneer_search_dropdown = (RegionX, RegionY, RegionWidth, RegionHeight)
-
-
-
+    bot_stage = 0
 
     cycler = True
     bot_stage = 0
@@ -83,8 +52,11 @@ def main():
             auctioneer_refresh_clickable_x = newWorldWindow.left + 635 + random.randint(0, 32)
             auctioneer_refresh_clickable_y = newWorldWindow.top + 310 + random.randint(0, 26)
 
-            auctioneer_search_clickable_x = newWorldWindow.left + 150 + random.randint(0, 300)
-            auctioneer_search_clickable_y = newWorldWindow.top + 321 + random.randint(0, 18)
+            # auctioneer_search_clickable_x = newWorldWindow.left + 150 + random.randint(0, 300)
+            # auctioneer_search_clickable_y = newWorldWindow.top + 321 + random.randint(0, 18)
+
+            auctioneer_search_clickable_x = newWorldWindow.left + 100 + random.randint(0, 489)
+            auctioneer_search_clickable_y = newWorldWindow.top + 296 + random.randint(0, 41)
 
 
             # Initial default state - reset any variables here
@@ -96,28 +68,35 @@ def main():
             elif bot_stage == 100:
                 print(bot_stage, " Click the search box")
                 pydirectinput.click(auctioneer_search_clickable_x, auctioneer_search_clickable_y)
+                time.sleep(random.randint(150, 340) / 1000)  # shorty distance
+                pydirectinput.click(auctioneer_search_clickable_x, auctioneer_search_clickable_y)
                 bot_stage = 101
 
             #pause for a quick second
             elif bot_stage == 101:
                 print(bot_stage, " A quick random wait timer")
                 time.sleep(random.randint(450, 800) / 1000)  # shorty distance
+                # cycler = False
                 bot_stage = 102
 
             # type "oil"
             elif bot_stage == 102:
-                print(bot_stage, " Type OIL")
-                pyautogui.press('o')
-                time.sleep(random.randint(110, 345) / 1000)  # shorty distance
-                pyautogui.press('i')
-                time.sleep(random.randint(110, 387) / 1000)  # shorty distance
-                pyautogui.press('l')
-                time.sleep(random.randint(201, 576) / 1000)  # shorty distance
+                print(bot_stage, " Type ", tp_items[search_term_id][1])
+
+                pyautogui.write(tp_items[search_term_id][1], interval=0.05)
+
+                # pyautogui.press('o')
+                # time.sleep(random.randint(110, 345) / 1000)  # shorty distance
+                # pyautogui.press('i')
+                # time.sleep(random.randint(110, 387) / 1000)  # shorty distance
+                # pyautogui.press('l')
+                # time.sleep(random.randint(201, 576) / 1000)  # shorty distance
                 bot_stage = 103
 
             # Locate "oil in drop down and click it
             elif bot_stage == 103:
-                search_dropdown_coords = pyautogui.locateOnScreen("imgs/auctioneer-search-dropdown-oil.png", grayscale=True, confidence=.75,
+                search_image_name = "imgs/search/" + tp_items[search_term_id][3]
+                search_dropdown_coords = pyautogui.locateOnScreen(search_image_name, grayscale=True, confidence=.75,
                                             region=region_auctioneer_search_dropdown)
                 if search_dropdown_coords is not None:
                     # print(search_dropdown_coords)
@@ -156,25 +135,6 @@ def main():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             # Find the Interactable "E"
             elif bot_stage == 1:
                 # Find that image on screen, in that region, with a confidence of 65%
@@ -207,6 +167,41 @@ def main():
         if keyboard.is_pressed('f1'):
             print("\nyou pressed F1, so resuming...")
             cycler = True
+
+def main():
+    """
+    Main function for the program
+    """
+    print("main function Initiated")
+
+
+    # Initiate windows class
+    windows_obj = Windows()
+
+    window = Tk()
+    window.title("New World Messages App")
+    window.geometry('250x500')
+
+    row_counter = 0
+    for item in tp_items:
+        print(item)
+        the_text = item[1], "#", row_counter
+        # btn = tk.Button(my_w, text=language, command=lambda lan=language:show_lan(lan))
+        # Button(window, text=the_text, command=lambda: clicked(windows_obj, row_counter)).grid(column=0, row=row_counter)
+        # Button(window, text=the_text, command=partial(clicked, windows_obj, row_counter)).grid(column=0, row=row_counter)
+        Button(window, text=the_text, command=lambda win=windows_obj, rc=row_counter: search_items(win, rc)).grid(column=0, row=row_counter)
+
+        # tk.Button(self.board, command=lambda i=i, j=j: self.on_click(i, j))
+
+        row_counter = row_counter + 1
+
+    # btn1 = Button(window, text="Oil", command=lambda: clicked(windows_obj, "Oil"))
+    # btn1.grid(column=0, row=0)
+    #
+    # btn2 = Button(window, text="Iron Ore", command=lambda: clicked(windows_obj, "Iron Ore"))
+    # btn2.grid(column=0, row=1)
+
+    window.mainloop()
 
 
 # Runs the main function
